75f251f9c12a338e6f0288600cd135a8103a6b0b
Abderahmane Mahdi Gharzouli <abderahmanemahdigharzouli@Abderahmanes-MacBook-Air.local>
2023-06-18 14:12:13 +0100

    Second commit

--- audioupload/urls.py
+++ audioupload/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/upload/', views.upload_audio, name='upload_audio'),
+     path('api/audio/<str:title>/', views.get_audio_by_title, name='get_audio_by_title'),
 ]
--- audioupload/views.py
+++ audioupload/views.py
@@ -1,6 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import AudioFile
+from .serializers import AudioFileSerializer
+@api_view(['GET'])
+def get_audio_by_title(request, title):
+    audio_files = AudioFile.objects.filter(title__icontains=title)
+    serializer = AudioFileSerializer(audio_files, many=True)
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def upload_audio(request):
